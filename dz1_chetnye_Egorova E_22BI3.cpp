871dcf9cb15f363f6b9962b31fb2755b1118ded2
Ekaterina Egorova <114211180+Katerina22BI3@users.noreply.github.com>
2023-12-01 23:39:41 +0300

    Add files via upload

---
+++ dz1_chetnye_Egorova E_22BI3.cpp
@@ -0,0 +1,335 @@
+﻿#include <iostream>
+#include <cmath>
+
+//tasks: 2,4,6,8,10,12,14,16,18,20
+double pi = 3.14529;
+//------------------------------------------------------
+/*Task 2
+double circumference_length(int r) //
+{
+    double Circum_lenght = 2 * pi * r;
+    return (Circum_lenght);
+}
+
+double circle_square(int r)
+{
+    double Circle_sqr = pi * r * r;
+    return (Circle_sqr);
+}
+*/
+//-------------------------------------------------------
+/*Task 4*/
+int sum_num(int num)
+{
+    int sum_num = 0;
+    for (int i = 4; i > 0; i--)
+    {
+        sum_num += num % 10;
+        num /= 10;
+    }
+    return (sum_num);
+}
+
+//-------------------------------------------------------
+/*Task 6
+double convertDegToRad(double degree)
+{
+    return (degree * (180 / pi));
+}
+double x_polarToDecart(int r, double corner)
+{
+    corner = convertDegToRad(corner);
+    double x;
+    x = r * std::cos(corner);
+    return (x);
+}
+double y_polarToDecart(int r, double corner)
+{
+    corner = convertDegToRad(corner);
+    double y;
+    y = r * std::sin(corner);
+    return (y);
+}
+*/
+
+//-------------------------------------------------------
+/*Task 8
+double medians(double a, double b, double c)
+{
+     return (0.5 * pow(2 * pow(c, 2) + 2 * pow(b,2) - pow(a, 2), 0.5));
+}
+
+bool checking_existence_triangle(double a, double b, double c)
+{
+    if ((a + b <= c) || (a + c <= b) || (b + c <= a))
+    {
+        return false;
+    }
+}
+*/
+//-------------------------------------------------------
+/*Task 10
+double isoscele_tringle(double a, double b, double c) //функция выяснения равнобедренности треугольника
+{
+    if (a == b || a == c || c == b) //проверяем условие признака равнобедренного треугольника
+    {
+        return 1;
+    }
+}
+
+bool checking_existence_triangle(double a, double b, double c)
+{
+    if ((a + b <= c) || (a + c <= b) || (b + c <= a))
+    {
+        return false;
+    }
+}*/
+
+//-------------------------------------------------------
+/*Task 12
+void recomendation(double weight, double height) //функция рекомендаций на основе роста и веса
+{
+
+    if (height - 100 == weight)
+    {
+        std::cout << "You have ideal weight!"; //идеальный вес
+    }
+    else if (height - 100 < weight)
+    {
+        std::cout << "We need to pump up..."; //надо подкачаться...
+    }
+    else if (height - 100 > weight) //если вес маленький
+    {
+        std::cout << "Eat more!"; //больше кушайте
+    }
+}
+*/
+//-------------------------------------------------------
+/*Task 14
+void price(int day, double phone_time)
+{
+    double tariph = 3.50; //пусть цена разговора 3.50 рублей в за минуту
+
+    if (day == 6 or day == 7)
+    {
+        std::cout << "20% discount today! The cost of your " << phone_time << "-minute conversation = " << tariph * phone_time * 0.8 << " dollars" << std::endl;
+    }
+    else
+    {
+        std::cout << "The cost of your " << phone_time << "-minute conversation = " << tariph * phone_time << " dollars" << std::endl;
+    }
+}
+*/
+//-------------------------------------------------------
+/*Task 16*/
+void ticket_verification(int number)
+{
+    int num123 = 0, num456 = 0; //инициализируем переменные для сумм первых трём и последних трёх цифр номера билета
+    for (int i = 0; i < 3; i++)
+    {
+        num123 += number % 10;
+        number /= 10;
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        num456 += number % 10;
+        number /= 10;
+    }
+    if (num123 == num456)
+    {
+        std::cout << "Your ticket is a lucky one!";
+    }
+    else
+    {
+        std::cout << "Your ticket is a unlucky!";
+    }
+}
+
+//-------------------------------------------------------
+/*Task 18
+void palindrom(int number)
+{
+    if (999 < number && number < 10000)
+    {
+        if ((number / 1000 == number % 10) && (number / 10) %10 == (number / 100) % 10)
+        {
+            std::cout << "It is palindrom\n";
+        }
+        else
+        {
+            std::cout << "It is not palindrom\n";
+        }
+    }
+    else
+    {
+        std::cout << "The entered number is not four digits.\n";
+    }
+}*/
+//-------------------------------------------------------
+/*Task 20
+void rectangle_check(float small_a, float small_b, float big_c, float big_d) //инициализируем 2 стороны маленького прямоугольника и 2 стороны большого прямоугольника
+{
+    if ((small_a < big_c && small_b < big_d) || (small_a < big_d && small_b < big_c))
+    {
+        std::cout << "A rectangle with sides a = " << small_a << " and b = " << small_b << " can be placed inside a rectangle with sides c = " << big_c << " and d = " << big_d << std::endl;
+    }
+    else
+    {
+        std::cout << "A rectangle with sides a = " << small_a << " and b = " << small_b << " cannot be placed inside a rectangle with sides c = " << big_c << " and d = " << big_d << std::endl;
+    }
+}
+*/
+
+int main()
+{
+    /*Task 2
+    int choose = 0; //инициализируем переменную для выбора нужного функционала пользователем с клавиатуры
+    int r; //объявляем переменную для радиуса окружности/круга
+    std::cout << "What do you want know: circle's square(1) or Circumference length(2)?\n";
+    std::cin >> choose;
+    if (choose == 1) //если пользователь захотел найти площадь круга
+    {
+        std::cout << "Enter radius of circle: ";
+        std::cin >> r;
+        std::cout << Circle_square(r) << std::endl;
+    }
+    else //если пользователь захотел найти длину окружности
+    {
+        std::cout << "Enter radius of circumference: ";
+        std::cin >> r;
+        std::cout << Circumference_length(r) << std::endl;
+    }
+    */
+
+    /*Task_4
+    int num = 0; //инициализируем переменную для хранения десятичного четырёхзначного числа с клавиатуры
+    std::cout << "Enter a four-digit decimal number: ";
+    std::cin >> num;
+    if (999 < num && num < 10000)
+    {
+        std::cout << "Summary digits of your number = " << sum_num(num) << std::endl;
+    }
+    else
+    {
+        std::cout << "The entered number is not four digits.\n";
+    }
+    */
+
+
+    /*Task 6
+    double r, corner; //объявляем переменные для радиус-вектора и угла между осью х и радиусом
+    std::cout << "Enter a radius-vect: ";
+    std::cin >> r;
+    std::cout << "Enter a corner between coordinate axes (in degrees): ";
+    std::cin >> corner;
+    double corner_rad = convertDegToRad(corner);
+    std::cout << "Decart coordinate:\n";
+    std::cout << "x = " << x_polarToDecart(r, corner_rad) << std::endl;
+    std::cout << "y = " << y_polarToDecart(r, corner_rad) << std::endl;
+    */
+
+    /*Task 8
+    double a=0, b=0, c=0; //инициализируем исходные стороны треугольника
+    double median_a = 0, median_b = 0, median_c = 0; //инициализируем стороны new треугольника
+    std::cout << "Enter the length of the first side of the triangle: ";
+    std::cin >> a;
+    std::cout << "Enter the length of the second side of the triangle: ";
+    std::cin >> b;
+    std::cout << "Enter the length of the third side of the triangle: ";
+    std::cin >> c;
+    if (checking_existence_triangle(a,b,c) == true) //если треугольник существует
+    {
+        median_a = medians(a, b, c); //ищем медиану к стороне а
+        median_b = medians(b, a, c); //ищем медиану к стороне b
+        median_c = medians(c, b, a); //ищем медиану к стороне c
+        std::cout << "Medians of internal triangle: " << medians(median_a, median_b, median_c) << ", " << medians(median_b, median_a, median_c) << ", " << medians(median_c, median_b, median_a) << std::endl;
+    }
+    else //если треугольник не существует
+    {
+        std::cout << "such a triangle does not exist\n";
+    }
+    */
+
+    /*Task 10
+    double a = 0, b = 0, c = 0; //инициализируем переменные: исходные стороны треугольника
+    std::cout << "Enter the length of the first side of the triangle: ";
+    std::cin >> a;
+    std::cout << "Enter the length of the second side of the triangle: ";
+    std::cin >> b;
+    std::cout << "Enter the length of the third side of the triangle: ";
+    std::cin >> c;
+    if (checking_existence_triangle(a, b, c) == true)
+    {
+        if (isoscele_tringle(a, b, c) == 1) //если условие равнобедренности из функции выполнилось
+        {
+            std::cout << "Your triangle is isosceles\n"; //вывод: треугольник равнобедренный
+        }
+        else //если условие равнобедренности из функции НЕ выполнилось
+        {
+            std::cout << "Your triangle is uneven\n"; //вывод: треугольник неравнобедренный
+        }
+    }
+    else //если треугольник не существует
+    {
+        std::cout << "such a triangle does not exist\n";
+    }
+    */
+
+    /*Task 12
+    double height = 0, weight = 0; //инициализируем переменные рост и вес человека
+    std::cout << "Enter the height: ";
+    std::cin >> height;
+    std::cout << "Enter the weight: ";
+    std::cin >> weight;
+
+    recomendation(weight, height);
+    */
+
+    /*tast 14
+    double phone_time = 0; //инициализируем переменную время разговора
+    int dayOfWeek; //инициализируем день недели
+    std::cout << "Enter the time of the conversation: ";
+    std::cin >> phone_time;
+    std::cout << "Enter the day of the week (number from 1 to 7. For example: '1' = Monday, '7' = Sunday: ";
+    std::cin >> dayOfWeek;
+
+    price(dayOfWeek, phone_time);
+    */
+
+    /*Task 16
+    int number_ticket = 0; //инициализируем переменную для номера билета
+    std::cout << "Enter the nubber of the ticket: ";
+    std::cin >> number_ticket;
+
+    if (number_ticket>99999 && number_ticket < 1000000)
+    {
+        ticket_verification(number_ticket);
+    }
+    else
+    {
+        std::cout << "Error. Not number of ticket\n";
+    }*/
+
+    /*Task 18
+    int number = 0; //инициализируем переменную для четырёхзначного числа
+    std::cout << "Enter a four - digit number : ";
+    std::cin >> number;
+
+    palindrom(number);
+    */
+
+    /*Task 20
+    int a, b, c, d; //объявляем переменные для сторон треугольника
+    std::cout << "Enter the first side of the first rectangle : ";
+    std::cin >> a;
+    std::cout << "Enter the second side of the first rectangle : ";
+    std::cin >> b;
+    std::cout << "Enter the first side of the second rectangle : ";
+    std::cin >> c;
+    std::cout << "Enter the second side of the second rectangle : ";
+    std::cin >> d;
+
+    rectangle_check(a, b, c, d);
+    */
+    return 0;
+}
---
+++ dz1_nechetnye_Erina_22BI3.cpp
@@ -0,0 +1,315 @@
+﻿#include <iostream>
+#include <math.h>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+    /*Task 1
+        std::cout << "Enter the first trapezoid base: "; //здесь и далее, просим пользователя ввести значение
+    int b1 = 0; //здесь и далее, инициализируем переменную
+    std::cin >> b1; //здесь и далее, записываем значение, которое ввёл пользователь
+    std::cout << "Enter the second trapezoid base: ";
+    int b2 = 0;
+    std::cin >> b2;
+    std::cout << "Enter a trapezoid height: ";
+    int h = 0;
+    std::cin >> h;
+    int square_trap = (b1 + b2) * h / 2; //считаем площадь трапеции
+    std::cout << "Trapezoid square = " << square_trap << std::endl; //здесь и далее, выводим результат
+
+    return 0;
+    */
+
+    /*Is it working? It works!
+    Enter the first trapezoid base : 5
+    Enter the second trapezoid base : 8
+    Enter a trapezoid height : 4
+    Trapezoid square = 26*/
+
+    /*Task 3
+    std::cout << "Enter the first cathetus: ";
+    int a = 0;
+    std::cin >> a;
+    std::cout << "Enter the second cathetus: ";
+    int b = 0;
+    std::cin >> b;
+    int square_tr = a * b / 2; //считаем площадь треугольника
+    int hyp = sqrt((a*a)+(b*b)); //считаем гипотенузу треугольника
+    std::cout << "Square = " << square_tr << std::endl;
+    std::cout << "Hypotenuse = " << hyp << std::endl;
+    return 0;*/
+
+    /*Is it working? It works!
+    Enter the first cathetus : 3
+    Enter the second cathetus : 4
+    Square = 6
+    Hypotenuse = 5*/
+
+    /*Task 5
+    std::cout << "Enter the first coordinate: ";
+    double x = 0;
+    std::cin >> x;
+    std::cout << "Enter the second coordinate: ";
+    double y = 0;
+    std::cin >> y;
+    double radius = sqrt((x * x) + (y * y)); //полярный радиус
+    double angle = atan(y / x); //полярные угол
+    std::cout << "Polar radius = " << radius << std::endl;
+    std::cout << "Polar angle = " << angle << " radian" << std::endl;
+    return 0;*/
+
+    /*Is it working? It works!
+    Enter the first coordinate : 5
+    Enter the second coordinate : 6
+    Polar radius = 7.81025
+    Polar angle = 0.876058 radian*/
+
+    /*Task 7
+    double a, b, c = 0;
+    std::cout << "Enter a: ";
+    std::cin >> a;
+    std::cout << "Enter b: ";
+    std::cin >> b;
+    std::cout << "Enter c: ";
+    std::cin >> c;
+    double d = b * b - 4 * a * c; //вычисляем дискриминант
+    if (d >= 0) //проверяем дискриминант, если он > 0 или = 0 то вычисляем корни(нь)
+    {
+        double x_1 = (-b + sqrt(d)) / (2 * a);
+        double x_2 = (-b - sqrt(d)) / (2 * a);
+        std::cout << "x_1 = " << x_1 << std::endl;
+        std::cout << "x_2 = " << x_2 << std::endl;
+    }
+    else //если дискриминант меньше нуля, то выводим ошибку
+    {
+        std::cout << "Error! Discriminant < 0" << std::endl;
+    }
+    return 0;*/
+    
+    /*Is it working? It works!
+    Case 1:
+    Enter a : 5
+    Enter b : 4
+    Enter c : 7
+    Error!Discriminant < 0
+
+    Case 2:
+    Enter a : -9
+    Enter b : 4
+    Enter c : 9
+    x_1 = -0.802172
+    x_2 = 1.24662*/
+
+    /*Task 9
+    std::cout << "Enter seconds: ";
+    double k = 0;
+    std::cin >> k;
+    if (k >= 0)
+    {
+        int hour = k/3600; //считаем сколько часов прошло
+        int min = (k - hour*3600)/60; // считаем сколько минут прошло (без часов)
+        std::cout << "Since the beginning of the day has passed " << hour << " hours" << " and " << min << " minutes." << std::endl;
+    }
+    else
+    {
+        std::cout << "Enter a positive value!" << std::endl;//на случай, если пользователь ввёл отрицательное значение
+    }
+    return 0;*/
+
+    /*Is it working? It works!
+    Case 1:
+    Enter seconds : -90
+    Enter a positive value!
+    Case 2:
+    Enter seconds: 50000
+    Since the beginning of the day has passed 13 hours and 53 minutes.*/
+
+    /*Task 11
+    std::cout << "Enter price: ";
+    int price = 0;
+    std::cin >> price;
+    if (price > 1000) //задаём условие получения скидки
+    {
+        float price_with_sale = price - price * 0.1;// рассчитываем стоимость со скидкой
+        std::cout << "Price including discount:" << price_with_sale << std::endl;
+    }
+    else //если сумма покупки меньше 1000
+    {
+        std::cout << "The purchase amount is less than 1000. No discount is provided." << std::endl;
+    }
+    return 0;*/
+
+    /*Is it working? It works!
+    Case 1:
+    Enter price: 15000
+    Price including discount: 13500
+    Case 2:
+    Enter price: 400
+    The purchase amount is less than 1000. No discount is provided.*/
+
+    /*Task 13
+    srand(time(0));
+    int a = (rand() % 9) + 1;//генерируем случайное число функцией rand() от 1 до 9
+    int b = (rand() % 9) + 1;
+    int mult = a * b;
+    std::cout << "Multiply " << a << " and " << b << std::endl;
+    int result;
+    std::cin >> result;
+    if (mult == result) //проверяем результат умножения которое ввел пользователь
+    {
+        std::cout << "Right!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Wrong! :(" << std::endl;
+    }
+    return 0;*/
+
+    /*Is it working? It works!
+    Case 1:
+    Multiply 4 and 1
+    4
+    Right!
+    Case 2:
+    Multiply 7 and 4
+    50
+    Wrong! :(
+    */
+
+    /*Task 15
+    std::cout << "Enter a number of month: ";
+    int month = 0;
+    std::cin >> month;
+    // далее сравниваем значение пользователя чтобы определить верный месяц и сезон
+    if (month == 1){
+        std::cout << "January. Winter.";
+    }
+    if (month == 2) {
+        std::cout << "February. Winter.";
+    }
+    if (month == 3) {
+        std::cout << "March. Spring.";
+    }
+    if (month == 4) {
+        std::cout << "April. Spring.";
+    }
+    if (month == 5) {
+        std::cout << "May. Spring.";
+    }
+    if (month == 6) {
+        std::cout << "June. Summer.";
+    }
+    if (month == 7) {
+        std::cout << "July. Summer.";
+    }
+    if (month == 8) {
+        std::cout << "August. Summer.";
+    }
+    if (month == 9) {
+        std::cout << "September. Autumn.";
+    }
+    if (month == 10) {
+        std::cout << "October. Autumn.";
+    }
+    if (month == 11) {
+        std::cout << "November. Autumn.";
+    }
+    if (month == 12) {
+        std::cout << "December. Winter.";
+    }
+    if (month<1 || month>12) //если пользователь ошибся
+    {
+        std::cout << "Mistake! Enter values from 1 to 12!" << std::endl;
+    }
+    return 0;*/
+
+    /*Is it working? It works!
+    Case 1:
+    Enter a number of month: 5
+    May. Spring.
+    Case 2:
+    Enter a number of month: 50
+    Mistake! Enter values from 1 to 12!*/
+
+    /*Task 17
+    setlocale(0, "rus"); //подключаем русскую раскладку
+    std::cout << "Enter a number from 1 to 99: ";
+    int num;
+    std::cin >> num;
+    int rest = num % 10; //находим остаток от деления (последнюю цифру числа)
+    if (num < 1 || num > 99) {
+        std::cout << "Mistake!Enter a number from 1 to 99." << std::endl;
+    }
+    else if (num == 11 || num == 12 || num == 13 || num == 14 || rest == 0 || rest >= 5 && rest <= 9) {
+        std::cout << num << " копеек" << std::endl;
+    }
+    else if (rest == 1) {
+        std::cout << num << " копейка" << std::endl;
+    }
+    else if (rest >= 2 && rest <= 4) {
+             std::cout << num << " копейки" << std::endl;
+    }
+    return 0; */
+
+    /*Is it working? It works!
+    Case 1:
+    Enter a number from 1 to 99: 55
+    55 копеек
+    Case 2:
+    Enter a number from 1 to 99: 1
+    1 копейка
+    Case 3:
+    Enter a number from 1 to 99: 22
+    22 копейки
+    Case 4:
+    Enter a number from 1 to 99: 554
+    Mistake!Enter a number from 1 to 99.*/
+
+    /*Task 19
+    std::cout << "Enter a three - digit number:" << std::endl;
+    int number;
+    std::cin >> number;
+    int fig1 = number / 100; //сотни
+    int fig2 = (number/10) % 10; //десятые
+    int fig3 = number % 10; //единицы
+    //19.1
+    int mult = fig1 * fig2 * fig3; //умножение цифр(сотни*десятки*единицы)
+    std::cout << "Enter number b:";
+    int b;
+    std::cin >> b;
+    if (b < mult) {
+        std::cout << "The number b is less than the result of multiplying" << std::endl;
+    }
+    if (b == mult) {
+        std::cout << "The number b and the result of multiplication are equal" << std::endl;
+    }
+    if (b > mult) {
+        std::cout << "The number b is greater than the result of multiplying" << std::endl;
+    }
+    //19.2
+    int sum = fig1 + fig2 + fig3; //сложение цифр
+    if (sum % 7 == 0) //проверка деления нацело на 7
+    {
+        std::cout << "The sum is a multiple of 7." << std::endl;
+    }
+    else {
+        std::cout << "The amount is not a multiple of 7." << std::endl;
+    }
+    return 0;*/
+
+    /*Is it working? It works!
+    Case 1:
+    Enter a three - digit number:
+    676
+    Enter number b:900
+    The number b is greater than the result of multiplying
+    The amount is not a multiple of 7.
+    Case 2:
+    Enter a three - digit number:
+    777
+    Enter number b:58
+    The number b is less than the result of multiplying
+    The sum is a multiple of 7.
+    */
+}
